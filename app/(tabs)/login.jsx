4c3bf69eda8d21ab37987b1724b1c519a03292f7
msantt <dejesusmarlon516@gmail.com>
2025-05-13 21:28:18 -0300

    ...
    ...

--- app/(tabs)/login.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
-import { NavigationContainer } from '@react-navigation/native';
-import TelaPrincipal from '../screens/HomeScreen';
-import TelaLogin from '../screens/LoginScreen';
-import TelaCadastro from '../screens/SignupScreen';
